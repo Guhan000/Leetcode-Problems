24c188aac6e7dd5d9479813b6ebf42c3d4c9fec8
Guhan <84968720+Guhan000@users.noreply.github.com>
2022-03-06 16:59:24 +0530

    Time: 1 ms (79.36%), Space: 43.8 MB (26.39%) - LeetHub

--- 81-search-in-rotated-sorted-array-ii/81-search-in-rotated-sorted-array-ii.java
+++ 81-search-in-rotated-sorted-array-ii/81-search-in-rotated-sorted-array-ii.java
@@ -1,10 +1,31 @@
 class Solution {
     public boolean search(int[] nums, int target) {
-        for(int i=0;i<nums.length;i++){
-            if(nums[i] == target){
-                return true;
+        
+        int l=0, h=nums.length-1;
+        while(l <= h){
+            int m = l+(h-l)/2;
+            if(nums[m] == target) return true;
+            
+            if(nums[m] < nums[h]){
+                //nums[mid..high] is pure
+                if(nums[m] < target && target <= nums[h]){
+                    l = m+1;
+                }else{
+                    h = m-1;
+                }
+            }else if(nums[m] > nums[h]){
+                //nums[low..mid] is pure
+                if(nums[l] <= target && target < nums[m]){
+                    h = m-1;
+                }else{
+                    l = m+1;
+                }
+            }else{
+                h--;
             }
         }
+        
         return false;
+        
     }
 }
