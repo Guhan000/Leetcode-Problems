e9eecebf3acbe1b5650c1ab1d502dd54b9bd5a65
Guhan <84968720+Guhan000@users.noreply.github.com>
2022-09-02 20:45:37 +0530

    Time: 91 ms (5.11%), Space: 25.8 MB (5.03%) - LeetHub

---
+++ 268-missing-number/268-missing-number.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        set<int> s;
+        for(int i=0;i<nums.size();i++){
+            s.insert(nums[i]);
+        }
+        for(int i=0;i<=s.size();i++){
+            if(s.find(i)==s.end()){
+                return i;
+            }
+        }
+        return -1;
+    }
+};
